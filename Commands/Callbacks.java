0f847d3d390a5e3de1b359e2537c717c001dd01c
tomik420z <107411943+tomik420z@users.noreply.github.com>
2022-06-14 19:09:24 +0300

    Delete Commands directory

--- Commands/Callbacks.java
+++
@@ -1,7 +0,0 @@
-package Commands;
-
-public class Callbacks {
-    public static final String JOKE_RATE = "JOKE_RATE";
-    public static final String ACCOUNT_DELETION = "ACCOUNT_DELETION";
-    public static final String DEFAULT = "DEFAULT";
-}
